d7b99f580b1c42618c2be8c8052d76171bc20db6
hcLee <newpoint03@naver.com>
2025-11-07 01:33:49 +0900

    로그인 뷰 및 액티비티 생성 회원가입은 아직 empty

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    viewBinding {
+        enable = true;
+    }
 }
 
 dependencies {
