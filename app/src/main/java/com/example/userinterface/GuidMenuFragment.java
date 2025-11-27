a409a75d6e3dc3996a89b34955b5251832f04a41
sumin-03 <doorcinema0000@gmail.com>
2025-11-28 02:44:18 +0900

    guide

---
+++ app/src/main/java/com/example/userinterface/GuidMenuFragment.java
@@ -0,0 +1,72 @@
+package com.example.userinterface;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GuidMenuFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GuidMenuFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public GuidMenuFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment GuidMenuFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static GuidMenuFragment newInstance(String param1, String param2) {
+        GuidMenuFragment fragment = new GuidMenuFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public static GuidMenuFragment newInstance(User user) {
+        GuidMenuFragment fragment = new GuidMenuFragment();
+        Bundle args = new Bundle();
+        args.putSerializable("USER_PROFILE", user);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_guid_menu, container, false);
+    }
+}
